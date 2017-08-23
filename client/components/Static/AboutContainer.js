c5222f1520b8713607b0e1544814819823afe5b2
Lamburger <rival2h@gmail.com>
2017-08-22 18:41:18 -0700

    typo

--- client/components/Static/AboutContainer.js
+++ client/components/Static/AboutContainer.js
@@ -9,7 +9,7 @@ class About extends Component {
             <h2>Welcome to the fishing blog!
             This is a simple blog where we fisherman can share our catches
             and fishing spots with other fisherman! Simply register and loginm,
-            then you will be able to add a post with a picture of your catche
+            then you will be able to add a post with a picture of your catch
             and the coordinates!</h2>
           </div>
         )
